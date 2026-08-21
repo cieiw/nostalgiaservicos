47f9ac7bb21d8ef3d6469b178b2712058c0a8e4c
cieiw <cieiwmail@gmail.com>
2026-08-21 13:04:52 -0300

    Evita cache do PWA bloquear painel mobile

--- admin/sw.js
+++ admin/sw.js
@@ -1,25 +1,7 @@
-const CACHE = 'nostalgia-admin-mobile-v1';
-const CORE = ['./', './index.html', './manifest.webmanifest', '../assets/nostalgia-logo.png'];
-
 self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(CORE)).then(() => self.skipWaiting()));
+  event.waitUntil(self.skipWaiting());
 });
 
 self.addEventListener('activate', event => {
-  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim()));
-});
-
-self.addEventListener('fetch', event => {
-  if (event.request.method !== 'GET') return;
-  const url = new URL(event.request.url);
-  // Dados e imagens autenticadas continuam sempre atuais e não ficam gravados
-  // no aparelho pelo cache do navegador.
-  if (url.hostname === 'api.nostalgiaservicos.site') return;
-  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
-    if (response.ok && url.origin === self.location.origin) {
-      const copy = response.clone();
-      caches.open(CACHE).then(cache => cache.put(event.request, copy));
-    }
-    return response;
-  }).catch(() => caches.match('./'))));
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(key => caches.delete(key)))).then(() => self.clients.claim()));
 });
